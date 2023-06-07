5d53ac59c6de63cf78ccce8463c762198707536d
saiful542 <saifuliiuc12@gmail.com>
2023-06-07 20:58:56 +0600

    updated

--- src/oop_Ts.ts
+++ src/oop_Ts.ts
@@ -1,8 +1,21 @@
 class People {
-  public getSalary = (name: "Abir", age: number, height: number): number => {
-    return height * age * 2 * 1000;
+  constructor(public name: string, public age: number, public height: number) {}
+  public getSalary = (): number => {
+    return this.height * this.age * 2 * 1000;
   };
 }
 
-const result = new People();
-console.log(result.getSalary("Abir", 25, 5.6));
+const resultOj = new People("Abir", 25, 5.6);
+// console.log(resultOj);
+
+class Student extends People {
+  constructor(name: string, age: number, height: number, public roll: number) {
+    super(name, age, height);
+    this.roll = roll;
+  }
+}
+
+const student_1 = new Student("Mr. Doctor", 18, 5.5, 12);
+console.log(student_1);
+
+
